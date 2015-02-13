d6c688f8a93d9a1acee75fe371855913220ec674
binarier <binarier@me.com>
2015-02-13 17:49:18 +0800

    hello world

--- src/main/java/com/github/rift/manager/Main.java
+++ src/main/java/com/github/rift/manager/Main.java
@@ -1,42 +1,49 @@
 package com.github.rift.manager;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.util.ssl.SslContextFactory;
-import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.spdy.server.http.HTTPSPDYServerConnector;
 
 public class Main
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
-	       Server server = new Server();
-	       
-	        SelectChannelConnector connector0 = new SelectChannelConnector();
-	        connector0.setPort(8080);
-	        connector0.setMaxIdleTime(30000);
-	        connector0.setRequestHeaderSize(8192);
-	 
-	        SelectChannelConnector connector1 = new SelectChannelConnector();
-	        connector1.setHost("127.0.0.1");
-	        connector1.setPort(8888);
-	        connector1.setThreadPool(new QueuedThreadPool(20));
-	        connector1.setName("admin");
-	 
-	        SslSelectChannelConnector ssl_connector = new SslSelectChannelConnector();
-	        String jetty_home = 
-	          System.getProperty("jetty.home","../jetty-distribution/target/distribution");
-	        System.setProperty("jetty.home",jetty_home);
-	        ssl_connector.setPort(8443);
-	        SslContextFactory cf = ssl_connector.getSslContextFactory();
-	        cf.setKeyStore(jetty_home + "/etc/keystore");
-	        cf.setKeyStorePassword("OBF:1vny1zlo1x8e1vnw1vn61x8g1zlu1vn4");
-	        cf.setKeyManagerPassword("OBF:1u2u1wml1z7s1z7a1wnl1u2g");
-	 
-	        server.setConnectors(new Connector[]{ connector0, connector1, ssl_connector });
-	 
-	        server.setHandler(new HelloHandler());
-	 
-	        server.start();
-	        server.join();
+		Server server = new Server();
+
+		HTTPSPDYServerConnector connector = new HTTPSPDYServerConnector(server);
+		connector.setPort(8080);
+
+		server.setConnectors(new Connector[]
+		{ connector });
+
+		server.setHandler(new AbstractHandler()
+		{
+
+			public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+			{
+			      // Declare response encoding and types
+		        response.setContentType("text/html; charset=utf-8");
+
+		        // Declare response status code
+		        response.setStatus(HttpServletResponse.SC_OK);
+
+		        // Write back response
+		        response.getWriter().println("<h1>Hello World</h1>");
+
+		        // Inform jetty that this request has now been handled
+		        baseRequest.setHandled(true);
+			}
+		});
+
+		server.start();
+		server.join();
 	}
 }
