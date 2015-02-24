7382a51e337313eb79d5604560e729747f787e1a
binarier <binarier@me.com>
2015-02-24 16:29:18 +0800

    first available

--- src/main/java/com/github/rift/manager/controller/Proxy.java
+++ src/main/java/com/github/rift/manager/controller/Proxy.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.userdetails.User;
@@ -11,6 +12,7 @@ import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.common.collect.Maps;
@@ -21,8 +23,20 @@ public class Proxy
 {
 	@Autowired
 	private JdbcTemplate jt;
+	
+	private String defaultRoute = 
+			"if (shExpMatch(host, '10.[0-9]+.[0-9]+.[0-9]+')) return d;\n" + 
+			"if (shExpMatch(host, '172.[0-9]+.[0-9]+.[0-9]+')) return d;\n" + 
+			"if (shExpMatch(host, '192.168.[0-9]+.[0-9]+')) return d;\n" + 
+			"if (url.indexOf('/complete/search?client=chrome-omni') != -1)\n" + 
+			"        return d;\n" + 
+			"if (url.indexOf('http://clients1.google.com/generate_204') == 0)\n" + 
+			"        return d;\n" + 
+			"if (url.indexOf('http://chart.apis.google.com/') == 0)\n" + 
+			"        return d;\n" + 
+			"if (url.indexOf('http://toolbarqueries.google.com') == 0)\n" + 
+			"        return d;\n";
 
-	@Transactional
 	@RequestMapping(value = "/data", produces = "application/json")
 	@ResponseBody
 	public Map<String, Object> data(@AuthenticationPrincipal User user)
@@ -42,8 +56,9 @@ public class Proxy
 		
 		script.append("function FindProxyForURL(url, host) {\n" +
 				"var p = 'PROXY 211.149.217.191:13128';\n" +
-				"if (shExpMatch(host, '*.google.*'))\n" +
-				"	return p;\n");
+				"var d = 'DIRECT';\n");
+		
+		script.append(defaultRoute);
 
 		script.append("var a = [");
 		boolean first = true;
@@ -71,5 +86,37 @@ public class Proxy
 			map.put(hostname, true);
 		}
 		return map;
+	}
+
+	@Transactional
+	@RequestMapping(value = "/add")
+	@ResponseBody
+	public void add(@AuthenticationPrincipal User user, @RequestParam String hostname)
+	{
+		String names[] = StringUtils.split(hostname, ",");
+		for (String name : names)
+		{
+			if (StringUtils.isBlank(name))
+				continue;
+			int count = jt.queryForObject("select count(*) from proxy_host where username = ? and hostname = ?", Integer.class, user.getUsername(), name);
+			if (count == 0)
+			{
+				jt.update("insert into proxy_host (username, hostname) values (?, ?)", user.getUsername(), name);
+			}
+		}
+	}
+	
+	@Transactional
+	@RequestMapping(value = "/remove")
+	@ResponseBody
+	public void remove(@AuthenticationPrincipal User user, @RequestParam String hostname)
+	{
+		String names[] = StringUtils.split(hostname, ",");
+		for (String name : names)
+		{
+			if (StringUtils.isBlank(name))
+				continue;
+			jt.update("delete from proxy_host where username = ? and hostname = ?", user.getUsername(), name);
+		}
 	}
 }
